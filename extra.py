fb975337468db4c9595c13d4ed3087e12b162928
roland31x <roland31x@gmail.com>
2024-10-21 19:01:00 +0000

    Add some extra functions

--- extra.py
+++ extra.py
@@ -1,47 +1,39 @@
-import math
-from collections import Counter
+import random
+import string
 
-def is_prime(num):
-    if num < 2: return False
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
-            return False
-    return True
+def random_string(length=10):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+def unique_elements(lst):
+    return list(set(lst))
+
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
     return a
 
-def lcm(a, b):
-    return a * b // gcd(a, b)
+def count_vowels(s):
+    return sum(c in 'aeiouAEIOU' for c in s)
 
-def flatten_dict(d, parent_key='', sep='.'):
-    items = []
-    for k, v in d.items():
-        new_key = f"{parent_key}{sep}{k}" if parent_key else k
-        if isinstance(v, dict):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
+def merge_dicts(*dicts):
+    result = {}
+    for d in dicts:
+        result.update(d)
+    return result
 
-def most_common(lst):
-    return Counter(lst).most_common(1)[0][0]
+def reverse_words(sentence):
+    return ' '.join(word[::-1] for word in sentence.split())
 
-def transpose(matrix):
-    return list(map(list, zip(*matrix)))
-
-def camel_to_snake(name):
-    import re
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-
-def swap_case(s):
-    return s.swapcase()
-
-def nested_sum(lst):
-    return sum(nested_sum(i) if isinstance(i, list) else i for i in lst)
-
-def digit_sum(num):
-    return sum(int(d) for d in str(abs(num)))
+def chunk_list(lst, chunk_size):
+    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
